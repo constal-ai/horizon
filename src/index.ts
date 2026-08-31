56992ba1cd08d40a0f3b023eddf62e4dd9071c93
Sidhartha Mani <sidharthamn@gmail.com>
2026-08-31 08:21:29 -0700

    chore: release Horizon 0.3.28

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { runHorizon } from "./workflow.js";
 
 export default agent({
   id: "horizon",
-  version: "0.3.27",
+  version: "0.3.28",
   model: "model",
   mode: "script",
   tools: TOOLS,
