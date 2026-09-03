a9980004465de6cd4317fefdf5f77efb2b7700a5
Sidhartha Mani <sidharthamn@gmail.com>
2026-09-03 16:16:40 -0700

    chore: release Horizon 0.6.22

--- src/index.ts
+++ src/index.ts
@@ -58,7 +58,7 @@ async function routeHorizon(message: unknown, ctx: Parameters<typeof runHorizon>
 
 export default agent({
   id: "horizon",
-  version: "0.6.21",
+  version: "0.6.22",
   model: "model",
   mode: "script",
   tools: TOOLS,
