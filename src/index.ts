68ebfe21c78360eab40511887b1d0f4bd1d484c8
Sidhartha Mani <sidharthamn@gmail.com>
2026-09-04 09:30:54 -0700

    release: bump Horizon to 0.6.35

--- src/index.ts
+++ src/index.ts
@@ -61,7 +61,7 @@ async function routeHorizon(message: unknown, ctx: Parameters<typeof runHorizon>
 
 export default agent({
   id: "horizon",
-  version: "0.6.34",
+  version: "0.6.35",
   model: "model",
   mode: "script",
   tools: TOOLS,
